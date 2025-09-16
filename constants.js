6060066fc0dcfed11c188e234c61761cec82ca7e
stefin <stefinracho@gmail.com>
2025-09-16 01:08:29 -0700

    add hide live chat

--- constants.js
+++ constants.js
@@ -6,8 +6,22 @@ export const HIDE_COMMENTCOUNT_CSS = `
     #less-replies .yt-core-attributed-string--white-space-no-wrap, /* watch, comments of a comment */
     #expander.ytd-comment-replies-renderer .dot.ytd-comment-replies-renderer, /* watch, author comment - right dot */
     #comments-button .yt-core-attributed-string--word-wrapping, /* shorts, comment button */
-    #contextual-info.ytd-engagement-panel-title-header-renderer /* shorts, comment dialog */ {
+    #contextual-info.ytd-engagement-panel-title-header-renderer, /* shorts, comment dialog */
+    #replies:has(#more-replies-icon) #more-replies /* watch, comments, button after creator pfp */ {
         display: none !important;
+    }
+
+    #more-replies .yt-spec-button-shape-next__icon, /* watch, comments, center chevron down icon */ 
+    #less-replies .yt-spec-button-shape-next__icon /* watch, comments, center chevron down icon */ {
+        margin: 0 !important;
+    }
+    
+    #more-replies button[aria-label*="repl"], /* watch, comments, transform to circle */ 
+    #less-replies button[aria-label*="repl"] /* watch, comments, transform to circle */ {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        margin-left: -8px;
     }`;
 
 export const HIDE_HOMEFEED_CSS = `
@@ -20,11 +34,21 @@ export const HIDE_LIKES_CSS = `
     .yt-spec-button-shape-next__button-text-content > .animated-rolling-number-wiz, /* watch, rating */
     button[title="I like this"] > .yt-spec-button-shape-next__button-text-content, /* watch, rating */
     #vote-count-middle, /* watch, comments */
-    #vote-count-middle.ytd-comment-action-buttons-renderer, /* search, latest posts */
+    #vote-count-middle.ytd-comment-action-buttons-renderer, /* search, latest posts from {youtuber} */
     #like-button.ytd-like-button-renderer .yt-core-attributed-string--word-wrapping, /* shorts, rating */
 
     /* 9-15-2025 */
     like-button-view-model .yt-spec-button-shape-next__button-text-content /* watch, rating */ {
+        display: none !important;
+    }
+
+    button[aria-label^="like this video"] .yt-spec-button-shape-next__icon /* watch, rating, center like button */ {
+        margin: 0 !important;
+    }`;
+
+export const HIDE_LIVECHAT_CSS = `
+    #chat-container, /* watch, live chat */
+    #teaser-carousel:has([aria-label*="Live chat"]) /* watch, near description */ {
         display: none !important;
     }`;
 
@@ -37,7 +61,7 @@ export const HIDE_PLAYABLES_CSS = `
     }`;
 
 export const HIDE_RELATEDVIDEOS_CSS = `
-    #related.style-scope.ytd-watch-flexy /* watch, related videos */ {
+    #related /* watch, related videos */ {
         display: none !important;
     }`;
 
@@ -48,7 +72,8 @@ export const HIDE_SHORTS_CSS = `
     #shorts-container, /* shorts, video */
 
     /* 9-18-2025 */
-    grid-shelf-view-model:has(ytm-shorts-lockup-view-model-v2, ytm-shorts-lockup-view-model) /* search, main */ {
+    grid-shelf-view-model:has(ytm-shorts-lockup-view-model-v2, ytm-shorts-lockup-view-model), /* search, main */ 
+    yt-tab-shape[tab-title="Shorts"] /* channel, tab-nav */ {
         display: none !important;
     }`;
 
@@ -73,7 +98,10 @@ export const HIDE_VIEWS_CSS = `
     ytd-rich-item-renderer .yt-content-metadata-view-model__delimiter, /* home, metadata-line, dot between views and date posted */
     yt-lockup-metadata-view-model .yt-content-metadata-view-model__metadata-row:has(.yt-content-metadata-view-model__delimiter) > span:first-child, /* watch, recommended videos */
     yt-lockup-metadata-view-model .yt-content-metadata-view-model__delimiter, /* watch, recommended videos metadata-line, dot between views and date posted */
-    ytd-watch-info-text:has(#view-count) #info > span:first-child, /* watch, main video metadata-line */
-    ytd-watch-info-text:has(#view-count) #info > span:first-child + span /* watch, main video metadata-line, whitespace after view count */ { 
+    ytd-watch-info-text:has(#view-count:not([aria-label*="watching"])) #info > span:first-child, /* watch, main video metadata-line, non-live */
+    ytd-watch-info-text:has(#view-count:not([aria-label*="watching"])) #info > span:first-child + span, /* watch, main video metadata-line, whitespace after view count and non-live */
+    #watch-card-subtitle, /* search, right of main (on large screens) */
+    yt-lockup-metadata-view-model .yt-content-metadata-view-model__metadata-row:has(.yt-content-metadata-view-model__delimiter) > span:first-child, /* watch, recommended, watching */
+    yt-lockup-view-model:has(.yt-badge-shape--thumbnail-live) .yt-content-metadata-view-model__metadata-row:nth-of-type(2) /* home, recommended, watching */ { 
         display: none !important;
     }`;
--- settings.js
+++ settings.js
@@ -1,43 +1,53 @@
 import {
-    HIDE_COMMENTCOUNT_CSS, HIDE_HOMEFEED_CSS, HIDE_LIKES_CSS,
-    HIDE_PLAYABLES_CSS, HIDE_RELATEDVIDEOS_CSS, HIDE_SHORTS_CSS,
-    HIDE_VIEWS_CSS,
+  HIDE_COMMENTCOUNT_CSS,
+  HIDE_HOMEFEED_CSS,
+  HIDE_LIKES_CSS,
+  HIDE_LIVECHAT_CSS,
+  HIDE_PLAYABLES_CSS,
+  HIDE_RELATEDVIDEOS_CSS,
+  HIDE_SHORTS_CSS,
+  HIDE_VIEWS_CSS,
 } from "./constants.js";
 
 export const settings = [
-    {
-        key: "isHiddenCommentCount",
-        checkboxId: "hide-commentcount-checkbox",
-        css: HIDE_COMMENTCOUNT_CSS
-    },
-    {
-        key: "isHiddenHomeFeed",
-        checkboxId: "hide-homefeed-checkbox",
-        css: HIDE_HOMEFEED_CSS
-    },
-    {
-        key: "isHiddenLikes",
-        checkboxId: "hide-likes-checkbox",
-        css: HIDE_LIKES_CSS
-    },
-    {
-        key: "isHiddenPlayables",
-        checkboxId: "hide-playables-checkbox",
-        css: HIDE_PLAYABLES_CSS
-    },
-    {
-        key: "isHiddenRelatedVideos",
-        checkboxId: "hide-relatedvideos-checkbox",
-        css: HIDE_RELATEDVIDEOS_CSS
-    },
-    {
-        key: "isHiddenShorts",
-        checkboxId: "hide-shorts-checkbox",
-        css: HIDE_SHORTS_CSS
-    },
-    {
-        key: "isHiddenViews",
-        checkboxId: "hide-views-checkbox",
-        css: HIDE_VIEWS_CSS
-    },
+  {
+    key: "isHiddenCommentCount",
+    checkboxId: "hide-commentcount-checkbox",
+    css: HIDE_COMMENTCOUNT_CSS,
+  },
+  {
+    key: "isHiddenHomeFeed",
+    checkboxId: "hide-homefeed-checkbox",
+    css: HIDE_HOMEFEED_CSS,
+  },
+  {
+    key: "isHiddenLikes",
+    checkboxId: "hide-likes-checkbox",
+    css: HIDE_LIKES_CSS,
+  },
+  {
+    key: "isHiddenLiveChat",
+    checkboxId: "hide-livechat-checkbox",
+    css: HIDE_LIVECHAT_CSS,
+  },
+  {
+    key: "isHiddenPlayables",
+    checkboxId: "hide-playables-checkbox",
+    css: HIDE_PLAYABLES_CSS,
+  },
+  {
+    key: "isHiddenRelatedVideos",
+    checkboxId: "hide-relatedvideos-checkbox",
+    css: HIDE_RELATEDVIDEOS_CSS,
+  },
+  {
+    key: "isHiddenShorts",
+    checkboxId: "hide-shorts-checkbox",
+    css: HIDE_SHORTS_CSS,
+  },
+  {
+    key: "isHiddenViews",
+    checkboxId: "hide-views-checkbox",
+    css: HIDE_VIEWS_CSS,
+  },
 ];
